5e8e2d089677b038757d53a46fe03ee24cdb846c
helgamartens0 <helgamartens@icloud.com>
2024-09-26 16:27:09 -0300

    Update index.js

--- INTEGRADOR/index.js
+++ INTEGRADOR/index.js
@@ -1,12 +1,17 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import translate from 'node-google-translate-skidz';
+import { franc } from 'franc';
+import fetch from 'node-fetch'; // Asegúrate de que esto esté correctamente importado
 
 const app = express();
 const port = 3000;
 
+const URL_DPTOS = "https://collectionapi.metmuseum.org/public/collection/v1/departments";
+
 app.use(express.static("public"));
 app.use(bodyParser.json());
+
 
 app.listen(port, () => {
     console.log(`Servidor escuchando en http://localhost:${port}`);
@@ -17,48 +22,17 @@ app.post("/traducir", async(req, res) => {
     const traducciones = {};
 
     try {
-        // Traducciones para cada campo
-        const tituloTraducido = await new Promise((resolve, reject) => {
-            translate({
-                text: titulo,
-                source: 'en',
-                target: 'es'
-            }, (err, result) => {
-                if (err) return reject(err);
-                resolve(result.translation); // Asegurarse de usar .translation
-            });
-        });
 
-        const dinastiaTraducida = await new Promise((resolve, reject) => {
-            translate({
-                text: dinastia,
-                source: 'en',
-                target: 'es'
-            }, (err, result) => {
-                if (err) return reject(err);
-                resolve(result.translation);
-            });
-        });
+        const tituloTraducido = await translate({ text: titulo, source: 'en', target: 'es' });
+        const dinastiaTraducida = await translate({ text: dinastia, source: 'en', target: 'es' });
+        const culturaTraducida = await translate({ text: cultura, source: 'en', target: 'es' });
 
-        const culturaTraducida = await new Promise((resolve, reject) => {
-            translate({
-                text: cultura,
-                source: 'en',
-                target: 'es'
-            }, (err, result) => {
-                if (err) return reject(err);
-                resolve(result.translation);
-            });
-        });
 
-        // Asignar traducciones al objeto de respuesta
-        traducciones.titulo = tituloTraducido;
-        traducciones.dinastia = dinastiaTraducida;
-        traducciones.cultura = culturaTraducida;
+        traducciones.titulo = tituloTraducido.translation;
+        traducciones.dinastia = dinastiaTraducida.translation;
+        traducciones.cultura = culturaTraducida.translation;
 
-        // Enviar respuesta
         res.json(traducciones);
-
     } catch (error) {
         console.error('Error al traducir:', error);
         res.status(500).json({ error: 'Error al traducir' });
